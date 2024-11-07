d6967073c11c9454f5494790800c4959b0254b8c
Darkspace-45 <bsrp2005@gmail.com>
2024-11-07 09:21:59 -0500

    Primera Parte (ACTUALIZADO)

--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientID = '0b2b68708e014e758475c58f52d41e51';
 const redirectUrl = 'http://localhost:3000';
@@ -6,3 +8,17 @@ const scopes = ["user-library-read", "playlist-read-private"];
 export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scopes.join(
     "%20"
 )}&response_type=token&show_dialog=true`;
+
+
+const apiClient = axios.create({
+    baseURL: "https://api.spotify.com/v1/",
+})
+
+export const setClientToken = (token) => {
+    apiClient.interceptors.request.use(async function(config) {
+        config.headers.Authorization = "Bearer " + token;
+        return config;
+    });
+};
+
+export default apiClient;
